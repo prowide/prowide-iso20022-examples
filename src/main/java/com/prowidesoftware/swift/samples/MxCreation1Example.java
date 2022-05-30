ec115b1619f64b9b0be9c00589010af8d478c2cc
zubri <sebastian@prowidesoftware.com>
2022-05-30 19:51:46 -0300

    dependency and message creation examples update

--- src/main/java/com/prowidesoftware/swift/samples/MxCreation1Example.java
+++ src/main/java/com/prowidesoftware/swift/samples/MxCreation1Example.java
@@ -15,32 +15,32 @@
  */
 package com.prowidesoftware.swift.samples;
 
-import com.prowidesoftware.swift.model.mx.MxPain00100108;
-import com.prowidesoftware.swift.model.mx.dic.CustomerCreditTransferInitiationV08;
-import com.prowidesoftware.swift.model.mx.dic.GroupHeader48;
-import com.prowidesoftware.swift.model.mx.dic.PartyIdentification43;
-import com.prowidesoftware.swift.model.mx.dic.PaymentInstruction22;
+import com.prowidesoftware.swift.model.mx.MxPain00100111;
+import com.prowidesoftware.swift.model.mx.dic.CustomerCreditTransferInitiationV11;
+import com.prowidesoftware.swift.model.mx.dic.GroupHeader95;
+import com.prowidesoftware.swift.model.mx.dic.PartyIdentification135;
+import com.prowidesoftware.swift.model.mx.dic.PaymentInstruction40;
 
 import java.math.BigDecimal;
 
 /**
- * This example shows how to create a new MX message using the Java model to set its content.<br>
+ * This example shows how to create a new MX message using the Java model to set its content. Using the default
+ * serialization options and no header.<br>
  * Outputs this:<br>
  *
  * <pre>
- * <?xml version="1.0" encoding="UTF-8"?>
- * <Doc:Document xmlns:Doc="urn:iso:std:iso:20022:tech:xsd:pain.001.001.08" xmlns:xsi="{http://www.w3.org/2000/xmlns/}Doc">
- *  <Doc:CstmrCdtTrfInitn>
- *   <Doc:GrpHdr>
- *    <Doc:CtrlSum>100</Doc:CtrlSum>
- *   </Doc:GrpHdr>
- *
- *   <Doc:PmtInf>
- *    <Doc:Dbtr>
- *     <Doc:Nm>foo</Doc:Nm>
- *    </Doc:Dbtr>
- *   </Doc:PmtInf>
- *  </Doc:CstmrCdtTrfInitn>
+ * <?xml version="1.0" encoding="UTF-8" ?>
+ * <Doc:Document xmlns:Doc="urn:iso:std:iso:20022:tech:xsd:pain.001.001.11">
+ *     <Doc:CstmrCdtTrfInitn>
+ *         <Doc:GrpHdr>
+ *             <Doc:CtrlSum>100</Doc:CtrlSum>
+ *         </Doc:GrpHdr>
+ *         <Doc:PmtInf>
+ *             <Doc:Dbtr>
+ *                 <Doc:Nm>foo</Doc:Nm>
+ *             </Doc:Dbtr>
+ *         </Doc:PmtInf>
+ *     </Doc:CstmrCdtTrfInitn>
  * </Doc:Document>
  * </pre>
  */
@@ -50,27 +50,27 @@ public class MxCreation1Example {
         /*
          * Initialize the MX object
          */
-        MxPain00100108 pain001001 = new MxPain00100108();
+        MxPain00100111 mx = new MxPain00100111();
 
         /*
          * Construct element content using the business dictionary
          */
-        PaymentInstruction22 pi = new PaymentInstruction22()
-                .setDbtr(new PartyIdentification43()
+        PaymentInstruction40 pi = new PaymentInstruction40()
+                .setDbtr(new PartyIdentification135()
                         .setNm("foo")
                 );
 
-        CustomerCreditTransferInitiationV08 ccti = new CustomerCreditTransferInitiationV08()
-                .setGrpHdr(new GroupHeader48()
+        CustomerCreditTransferInitiationV11 ccti = new CustomerCreditTransferInitiationV11()
+                .setGrpHdr(new GroupHeader95()
                         .setCtrlSum(new BigDecimal(100))
                 )
                 .addPmtInf(pi);
 
-        pain001001.setCstmrCdtTrfInitn(ccti);
+        mx.setCstmrCdtTrfInitn(ccti);
 
         /*
          * Print the generated message in its XML format
          */
-        System.out.println(pain001001.message());
+        System.out.println(mx.message());
     }
 }
--- src/main/java/com/prowidesoftware/swift/samples/MxCreation2Example.java
+++ src/main/java/com/prowidesoftware/swift/samples/MxCreation2Example.java
@@ -15,7 +15,8 @@
  */
 package com.prowidesoftware.swift.samples;
 
-import com.prowidesoftware.swift.model.mx.MxPacs00800102;
+import com.prowidesoftware.swift.model.mx.MxPacs00800108;
+import com.prowidesoftware.swift.model.mx.MxWriteConfiguration;
 import com.prowidesoftware.swift.model.mx.dic.*;
 
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -29,102 +30,82 @@ import java.util.GregorianCalendar;
  * <p>
  * Outputs this MX message:<br>
  * <pre>
- * <?xml version="1.0" encoding="UTF-8"?>
- * <Doc:Document xmlns:Doc="urn:iso:std:iso:20022:tech:xsd:pacs.008.001.02" xmlns:xsi="{http://www.w3.org/2000/xmlns/}Doc">
- *  <Doc:FIToFICstmrCdtTrf>
- *   <Doc:GrpHdr>
- *    <Doc:MsgId>TBEXO12345</Doc:MsgId>
- *
- *    <Doc:CreDtTm>2015-05-19T13:15:23.336-03:00</Doc:CreDtTm>
- *
- *    <Doc:NbOfTxs>1</Doc:NbOfTxs>
- *
- *    <Doc:SttlmInf>
- *     <Doc:SttlmMtd>INDA</Doc:SttlmMtd>
- *
- *     <Doc:SttlmAcct>
- *      <Doc:Id>
- *       <Doc:Othr>
- *        <Doc:Id>00010013800002001234</Doc:Id>
- *       </Doc:Othr>
- *      </Doc:Id>
- *     </Doc:SttlmAcct>
- *    </Doc:SttlmInf>
- *
- *    <Doc:InstgAgt>
- *     <Doc:FinInstnId>
- *      <Doc:BIC>FOOBARC0XXX</Doc:BIC>
- *     </Doc:FinInstnId>
- *    </Doc:InstgAgt>
- *
- *    <Doc:InstdAgt>
- *     <Doc:FinInstnId>
- *      <Doc:BIC>BANKANC0XXX</Doc:BIC>
- *     </Doc:FinInstnId>
- *    </Doc:InstdAgt>
- *   </Doc:GrpHdr>
- *
- *   <Doc:CdtTrfTxInf>
- *    <Doc:PmtId>
- *     <Doc:InstrId>TBEXO12345</Doc:InstrId>
- *
- *     <Doc:EndToEndId>TBEXO12345</Doc:EndToEndId>
- *
- *     <Doc:TxId>TBEXO12345</Doc:TxId>
- *    </Doc:PmtId>
- *
- *    <Doc:IntrBkSttlmAmt Ccy="USD" >23453</Doc:IntrBkSttlmAmt>
- *
- *    <Doc:IntrBkSttlmDt>2015-05-19T13:15:23.352-03:00</Doc:IntrBkSttlmDt>
- *
- *    <Doc:ChrgBr>DEBT</Doc:ChrgBr>
- *
- *    <Doc:Dbtr>
- *     <Doc:Nm>JOE DOE</Doc:Nm>
- *
- *     <Doc:PstlAdr>
- *      <Doc:AdrLine>310 Field Road, NY</Doc:AdrLine>
- *     </Doc:PstlAdr>
- *    </Doc:Dbtr>
- *
- *    <Doc:DbtrAcct>
- *     <Doc:Id>
- *      <Doc:Othr>
- *       <Doc:Id>01111001759234567890</Doc:Id>
- *      </Doc:Othr>
- *    </Doc:Id>
- *    </Doc:DbtrAcct>
- *
- *    <Doc:DbtrAgt>
- *     <Doc:FinInstnId>
- *      <Doc:BIC>FOOBARC0XXX</Doc:BIC>
- *     </Doc:FinInstnId>
- *    </Doc:DbtrAgt>
- *
- *    <Doc:CdtrAgt>
- *     <Doc:FinInstnId>
- *      <Doc:BIC>BANKANC0XXX</Doc:BIC>
- *     </Doc:FinInstnId>
- *    </Doc:CdtrAgt>
- *
- *    <Doc:Cdtr>
- *     <Doc:Nm>TEST CORP</Doc:Nm>
- *
- *     <Doc:PstlAdr>
- *      <Doc:AdrLine>Nellis ABC, NV</Doc:AdrLine>
- *     </Doc:PstlAdr>
- *    </Doc:Cdtr>
- *
- *    <Doc:CdtrAcct>
- *     <Doc:Id>
- *      <Doc:Othr>
- *       <Doc:Id>00013500510020179998</Doc:Id>
- *      </Doc:Othr>
- *     </Doc:Id>
- *    </Doc:CdtrAcct>
- *   </Doc:CdtTrfTxInf>
- *  </Doc:FIToFICstmrCdtTrf>
- * </Doc:Document>
+ * <?xml version="1.0" encoding="UTF-8" ?>
+ * <Document xmlns="urn:iso:std:iso:20022:tech:xsd:pacs.008.001.08">
+ *     <FIToFICstmrCdtTrf>
+ *         <GrpHdr>
+ *             <MsgId>TBEXO12345</MsgId>
+ *             <CreDtTm>2022-05-30T19:41:37.724-03:00</CreDtTm>
+ *             <NbOfTxs>1</NbOfTxs>
+ *             <SttlmInf>
+ *                 <SttlmMtd>INDA</SttlmMtd>
+ *                 <SttlmAcct>
+ *                     <Id>
+ *                         <Othr>
+ *                             <Id>00010013800002001234</Id>
+ *                         </Othr>
+ *                     </Id>
+ *                 </SttlmAcct>
+ *             </SttlmInf>
+ *             <InstgAgt>
+ *                 <FinInstnId>
+ *                     <BICFI>FOOBARC0XXX</BICFI>
+ *                 </FinInstnId>
+ *             </InstgAgt>
+ *             <InstdAgt>
+ *                 <FinInstnId>
+ *                     <BICFI>BANKANC0XXX</BICFI>
+ *                 </FinInstnId>
+ *             </InstdAgt>
+ *         </GrpHdr>
+ *         <CdtTrfTxInf>
+ *             <PmtId>
+ *                 <InstrId>TBEXO12345</InstrId>
+ *                 <EndToEndId>TBEXO12345</EndToEndId>
+ *                 <TxId>TBEXO12345</TxId>
+ *             </PmtId>
+ *             <IntrBkSttlmAmt Ccy="USD">23453</IntrBkSttlmAmt>
+ *             <IntrBkSttlmDt>2022-05-30</IntrBkSttlmDt>
+ *             <ChrgBr>DEBT</ChrgBr>
+ *             <Dbtr>
+ *                 <Nm>JOE DOE</Nm>
+ *                 <PstlAdr>
+ *                     <AdrLine>310 Field Road, NY</AdrLine>
+ *                 </PstlAdr>
+ *             </Dbtr>
+ *             <DbtrAcct>
+ *                 <Id>
+ *                     <Othr>
+ *                         <Id>01111001759234567890</Id>
+ *                     </Othr>
+ *                 </Id>
+ *             </DbtrAcct>
+ *             <DbtrAgt>
+ *                 <FinInstnId>
+ *                     <BICFI>FOOBARC0XXX</BICFI>
+ *                 </FinInstnId>
+ *             </DbtrAgt>
+ *             <CdtrAgt>
+ *                 <FinInstnId>
+ *                     <BICFI>BANKANC0XXX</BICFI>
+ *                 </FinInstnId>
+ *             </CdtrAgt>
+ *             <Cdtr>
+ *                 <Nm>TEST CORP</Nm>
+ *                 <PstlAdr>
+ *                     <AdrLine>Nellis ABC, NV</AdrLine>
+ *                 </PstlAdr>
+ *             </Cdtr>
+ *             <CdtrAcct>
+ *                 <Id>
+ *                     <Othr>
+ *                         <Id>00013500510020179998</Id>
+ *                     </Othr>
+ *                 </Id>
+ *             </CdtrAcct>
+ *         </CdtTrfTxInf>
+ *     </FIToFICstmrCdtTrf>
+ * </Document>
  * </pre><br><br>
  * <p>
  * This MX messages is the equivalent to the MT103 simple credit transfer:
@@ -152,12 +133,12 @@ public class MxCreation2Example {
         /*
          * Initialize the MX object
          */
-        MxPacs00800102 mx = new MxPacs00800102();
+        MxPacs00800108 mx = new MxPacs00800108();
 
         /*
          * Initialize main message content main objects
          */
-        mx.setFIToFICstmrCdtTrf(new FIToFICustomerCreditTransferV02().setGrpHdr(new GroupHeader33()));
+        mx.setFIToFICstmrCdtTrf(new FIToFICustomerCreditTransferV08().setGrpHdr(new GroupHeader93()));
 
         /*
          * General Information
@@ -169,10 +150,10 @@ public class MxCreation2Example {
         /*
          * Settlement Information
          */
-        mx.getFIToFICstmrCdtTrf().getGrpHdr().setSttlmInf(new SettlementInformation13());
+        mx.getFIToFICstmrCdtTrf().getGrpHdr().setSttlmInf(new SettlementInstruction7());
         mx.getFIToFICstmrCdtTrf().getGrpHdr().getSttlmInf().setSttlmMtd(SettlementMethod1Code.INDA);
         mx.getFIToFICstmrCdtTrf().getGrpHdr().getSttlmInf().setSttlmAcct(
-                (new CashAccount16()).setId(
+                (new CashAccount38()).setId(
                         (new AccountIdentification4Choice()).setOthr(
                                 (new GenericAccountIdentification1()).setId("00010013800002001234"))));
 
@@ -180,25 +161,25 @@ public class MxCreation2Example {
          * Instructing Agent
          */
         mx.getFIToFICstmrCdtTrf().getGrpHdr().setInstgAgt(
-                (new BranchAndFinancialInstitutionIdentification4()).setFinInstnId(
-                        (new FinancialInstitutionIdentification7()).setBIC("FOOBARC0XXX")));
+                (new BranchAndFinancialInstitutionIdentification6()).setFinInstnId(
+                        (new FinancialInstitutionIdentification18()).setBICFI("FOOBARC0XXX")));
 
         /*
          * Instructed Agent
          */
         mx.getFIToFICstmrCdtTrf().getGrpHdr().setInstdAgt(
-                (new BranchAndFinancialInstitutionIdentification4()).setFinInstnId(
-                        (new FinancialInstitutionIdentification7()).setBIC("BANKANC0XXX")));
+                (new BranchAndFinancialInstitutionIdentification6()).setFinInstnId(
+                        (new FinancialInstitutionIdentification18()).setBICFI("BANKANC0XXX")));
 
         /*
          * Payment Transaction Information
          */
-        CreditTransferTransactionInformation11 cti = new CreditTransferTransactionInformation11();
+        CreditTransferTransaction39 cti = new CreditTransferTransaction39();
 
         /*
          * Transaction Identification
          */
-        cti.setPmtId(new PaymentIdentification3());
+        cti.setPmtId(new PaymentIdentification7());
         cti.getPmtId().setInstrId("TBEXO12345");
         cti.getPmtId().setEndToEndId("TBEXO12345");
         cti.getPmtId().setTxId("TBEXO12345");
@@ -224,50 +205,53 @@ public class MxCreation2Example {
         /*
          * Orderer Name & Address
          */
-        cti.setDbtr(new PartyIdentification32());
+        cti.setDbtr(new PartyIdentification135());
         cti.getDbtr().setNm("JOE DOE");
-        cti.getDbtr().setPstlAdr((new PostalAddress6()).addAdrLine("310 Field Road, NY"));
+        cti.getDbtr().setPstlAdr((new PostalAddress24()).addAdrLine("310 Field Road, NY"));
 
         /*
          * Orderer Account
          */
         cti.setDbtrAcct(
-                (new CashAccount16()).setId(
+                (new CashAccount38()).setId(
                         (new AccountIdentification4Choice()).setOthr(
                                 (new GenericAccountIdentification1()).setId("01111001759234567890"))));
         /*
          * Order Financial Institution
          */
         cti.setDbtrAgt(
-                (new BranchAndFinancialInstitutionIdentification4()).setFinInstnId(
-                        (new FinancialInstitutionIdentification7()).setBIC("FOOBARC0XXX")));
+                (new BranchAndFinancialInstitutionIdentification6()).setFinInstnId(
+                        (new FinancialInstitutionIdentification18()).setBICFI("FOOBARC0XXX")));
 
         /*
          * Beneficiary Institution
          */
-        cti.setCdtrAgt((new BranchAndFinancialInstitutionIdentification4()).setFinInstnId((new FinancialInstitutionIdentification7()).setBIC("BANKANC0XXX")));
+        cti.setCdtrAgt((new BranchAndFinancialInstitutionIdentification6()).setFinInstnId(
+                (new FinancialInstitutionIdentification18()).setBICFI("BANKANC0XXX")));
 
         /*
          * Beneficiary Name & Address
          */
-        cti.setCdtr(new PartyIdentification32());
+        cti.setCdtr(new PartyIdentification135());
         cti.getCdtr().setNm("TEST CORP");
-        cti.getCdtr().setPstlAdr((new PostalAddress6().addAdrLine("Nellis ABC, NV")));
+        cti.getCdtr().setPstlAdr((new PostalAddress24().addAdrLine("Nellis ABC, NV")));
 
         /*
          * Beneficiary Account
          */
         cti.setCdtrAcct(
-                (new CashAccount16()).setId(
+                (new CashAccount38()).setId(
                         (new AccountIdentification4Choice()).setOthr(
                                 (new GenericAccountIdentification1()).setId("00013500510020179998"))));
 
         mx.getFIToFICstmrCdtTrf().addCdtTrfTxInf(cti);
 
         /*
-         * Print the generated message in its XML format
+         * Print the generated message in its XML format (without prefix)
          */
-        System.out.println(mx.message());
+        MxWriteConfiguration conf = new MxWriteConfiguration();
+        conf.documentPrefix = null;
+        System.out.println(mx.message(conf));
     }
 
     public static XMLGregorianCalendar getXMLGregorianCalendarNow() {
